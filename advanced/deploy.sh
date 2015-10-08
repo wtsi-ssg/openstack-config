5d33e556ccc2a8d0fbd2bd75d018d76d91f5dde7
Dave Holland <dh3@sanger.ac.uk>
2015-10-08 10:45:35 +0100

    make rocket go!

---
+++ advanced/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COMPUTESCALE=7
+CONTROLSCALE=3
+CEPHSCALE=3
+cd ~stack
+. stackrc
+openstack overcloud deploy --templates ~/templates/my-overcloud -e ~/templates/my-overcloud/environments/network-isolation.yaml -e ~/templates/network-environment.yaml -e ~/templates/storage-environment.yaml --control-scale $CONTROLSCALE --compute-scale $COMPUTESCALE --ceph-storage-scale $CEPHSCALE --control-flavor control --compute-flavor compute --ceph-storage-flavor ceph-storage --ntp-server pool.ntp.org --neutron-network-type vxlan --neutron-tunnel-types vxlan --neutron-bridge-mappings datacentre:br-ex,br-eth1:br-eth1 --neutron-network-vlan-ranges datacentre:1:1000,br-eth1:FLOATVLAN:FLOATVLAN

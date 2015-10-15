23243630d0d79eeb3fe0b6bbfd821295eb6ed41b
Dave Holland <dh3@sanger.ac.uk>
2015-10-15 14:20:27 +0100

    disk-heavy flavours for kr2

--- support_scripts/create_flavors.sh
+++ support_scripts/create_flavors.sh
@@ -2,7 +2,12 @@
 
 # we try to follow Amazon's naming scheme
 # https://aws.amazon.com/ec2/instance-types/
-
+# global flavors are numbered 1000+
 nova flavor-create c1.large   1000 4096  8 2
 nova flavor-create c1.xlarge  1001 8192  8 4
 nova flavor-create c1.2xlarge 1002 16384 8 8
+
+# project-specific flavors are numbered 2000+
+# disk-heavy flavo(u)rs for kr2: 32GB RAM, 400GB root disk
+nova flavor-create d1.2xlarge 2000 32768 400 8
+nova flavor-create d1.4xlarge 2001 32768 400 16

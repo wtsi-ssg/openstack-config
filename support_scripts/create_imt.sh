58b5fd13b0e02089324121b1bd55af7b39c25c52
Dave Holland <dh3@sanger.ac.uk>
2015-11-12 10:19:55 +0000

    add Luke

--- support_scripts/create_imt.sh
+++ support_scripts/create_imt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TENANT=ssg-imt INSTANCES=64 CORES=96 RAM=65536 FLOATINGIPS=15 VOLUMEGB=100 VOLUMES=10   ./create_tenant_group.sh 
-for i in jg9
+#TENANT=ssg-imt INSTANCES=64 CORES=96 RAM=65536 FLOATINGIPS=15 VOLUMEGB=100 VOLUMES=10   ./create_tenant_group.sh 
+for i in jg9 lfb
 do
   USERACCOUNT="$i" TENANT="$i"           ./create_user_and_tenant.sh
   USERACCOUNT="$i" TENANT="ssg-imt"      ./add_user_to_tenant.sh
